bc0edcc361e01a7beb135175d21a6e034c492e3f
Gavin Carothers <gavin@carothers.name>
2011-12-31 12:46:38 -0800

    Working encrypt and decrypt functions

--- payment/payment/models.py
+++ payment/payment/models.py
@@ -2,6 +2,7 @@ from sqlalchemy import (
     Column,
     Integer,
     Text,
+    ForeignKey,
     )
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -9,20 +10,59 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import (
     scoped_session,
     sessionmaker,
+    relationship,
+    backref
     )
 
 from zope.sqlalchemy import ZopeTransactionExtension
 
+
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
-class MyModel(Base):
-    __tablename__ = 'models'
+class Purchase(Base):
+    __tablename__ = 'purchases'
     id = Column(Integer, primary_key=True)
-    name = Column(Text, unique=True)
-    value = Column(Integer)
+    item_id = Column(Integer, ForeignKey('items'))
+    customer_email = Column(Text)
 
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
+    item = relationship("Item", backref=backref('purchases'))
 
+    def __str__(self):
+        return "{id:x}:{item}:{email}".format(id=self.id,
+                                              item=self.item,
+                                              email=self.customer_email)
+
+
+    def encrypted_token(self):
+        plain_text = str(self)
+        key = self.item.key
+        return encrypt(key, plain_text)
+
+
+def encrypt(key, plain_text):
+    from Crypto.Cipher import AES
+    from Crypto.Random import get_random_bytes
+    import base64
+    pad = AES.block_size - len(plain_text) % AES.block_size 
+    data = plain_text + pad * chr(pad)
+    iv_bytes = get_random_bytes(AES.block_size)
+    key_bytes = key
+    encrypted_bytes = iv_bytes + AES.new(key_bytes, AES.MODE_CBC, iv_bytes).encrypt(data)
+    encrypted_string = base64.urlsafe_b64encode(str(encrypted_bytes))
+    return encrypted_string
+
+def decrypt(key, encrypted_string):
+    from Crypto.Cipher import AES
+    import base64
+    encrypted_bytes = base64.urlsafe_b64decode(encrypted_string)
+    iv_bytes = encrypted_bytes[:AES.block_size]
+    encrypted_bytes = encrypted_bytes[AES.block_size:]
+    plain_text = AES.new(key, AES.MODE_CBC, iv_bytes).decrypt(encrypted_bytes)
+    pad = ord(plain_text[-1])
+    plain_text = plain_text[:-pad]
+    return plain_text
+
+class Item(Base):
+    id = Column(Integer, primary_key=True)
+    key = Column(Text, unique=True)
